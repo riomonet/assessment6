fcc10ece436b8e048ab272541eba2164db0165ca
riomonet <arizablozki@gmail.com>
2023-07-03 15:27:26 -0400

    fix and refactor broken-app app.js

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 let axios = require('axios');
-var app = express();
+const app = express();
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+app.use(express.json())
 
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
-  }
-});
 
+app.post('/', (req, res, next) => {
+
+    let urls = req.body.developers.map(dev => `https://api.github.com/users/${dev}`)
+    const requests = urls.map(url => axios.get(url))
+	 
+    Promise.all(requests)
+	.then(responses => {
+	    let out = responses.map(r => {
+		return { name: r.data.name, bio: r.data.bio }
+	    })
+	    return res.send(JSON.stringify(out));
+	})
+	.catch(error => res.send("ERROR: bad input"))
+})
+     
 app.listen(3000);
+    
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,5 @@
 # Broken App Issues
+The problem with the broken app was, the app was trying to access the results variable, before all
+the promises were fulfilled. 
+To fix it I refactored the code to wait for all the promises to be fullfilled prior to 
+accessing the variable and sending the results to the user.
